ccfa8ada045b4408cb1ac8f500172c9760441d60
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-12-16 11:15:31 -0500

    .

---
+++ PythonBatch/PythonBatchDeploy/DeployBatchComponents.ps1
@@ -0,0 +1,34 @@
+#	Many components under an Azure Batch account can't be provisioned via an ARM template
+#	yet.  For this reason we use this PowerShell script after running the ARM template
+
+###  Replace the following variable values with yours
+
+#  Resource group name where the batch account is deployed
+$rg = "PythonBatch"
+#  Name of the batch account as specified when running the ARM template
+$batchAccount = "vplbatch"
+
+###  Those variable values do not need to be changed
+$appID = "PythonScript"
+$packageUrl = "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/AppPackages/Sample.zip"
+$appVersion = "1.0"
+
+#  Create the batch application
+New-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $appID
+
+#  Copy the package locally
+wget $packageUrl > Sample.zip
+
+#  Upload the batch application package
+New-AzureRmBatchApplicationPackage -ResourceGroupName $rg -AccountName $batchAccount `
+    -ApplicationVersion $appVersion -ApplicationId $appID -Format zip -FilePath Sample.zip
+
+#  Set default version of the application
+Set-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $appID `
+    -DefaultVersion $appVersion
+
+#  Clean local copy of the package
+rm Sample.zip
+
+#  Remove-AzureRmBatchApplicationPackage -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $appID -ApplicationVersion $appVersion
+#  Remove-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $appID
