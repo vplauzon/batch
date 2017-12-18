86af3a63ea472f9a9e0d511c2356796536e6964e
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-12-18 08:59:53 -0500

    .

--- PythonBatch/PythonBatchDeploy/DeployBatchComponents.ps1
+++ PythonBatch/PythonBatchDeploy/DeployBatchComponents.ps1
@@ -11,17 +11,10 @@ $batchAccount = "vplbatch"
 $storageAccount = "vplbatchsto"
 
 ###  Those variable values do not need to be changed
-
-###  Configure Batch account
-
-###  Configure Storage account
-
-#  Fetch storage account access keys
-$keys = Get-AzureRmStorageAccountKey -ResourceGroupName $rg -Name $storageAccount
-#  Create a context for storage account commands
-$context = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $keys.Value[0]
-#  Create a container for resource files
-New-AzureStorageContainer -Name resources -Context $context
+$pythonAppID = "PythonScript"
+$pythonAppVersion = "1.0"
+$setupAppID = "PythonSetup"
+$setupAppVersion = "1.0"
 
 #  Create a folder to copy resources locally
 mkdir tmp-resources
@@ -32,14 +25,42 @@ wget "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy
 wget "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/Resources/setup-python.sh" `
     > tmp-resources/setup-python.sh
 
-#  Copy resources to blob storage
-Set-AzureStorageBlobContent -File "tmp-resources/sample.py" `
-    -Container resources -Blob "sample.py" -BlobType Block -Force -Context $context
-Set-AzureStorageBlobContent -File "tmp-resources/setup-python.sh" `
-    -Container resources -Blob "setup-python.sh" -BlobType Block -Force -Context $context
+#  Build an archive for each resource separately
+Compress-Archive -Path "tmp-resources\sample.py" -DestinationPath "tmp-resources/sample.zip" `
+    -CompressionLevel Optimal
+Compress-Archive -Path "tmp-resources\setup-python.sh" -DestinationPath "tmp-resources/setup-python.zip" `
+    -CompressionLevel Optimal
 
-#  Clean temporary folder
-rmdir tmp-resources -Force -Recurse
+###  Configure Batch account
+
+#  Create 2 batch applications
+New-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $pythonAppID
+New-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $setupAppID
+
+#  Upload 2 batch application packages
+New-AzureRmBatchApplicationPackage -ResourceGroupName $rg -AccountName $batchAccount `
+    -ApplicationVersion $pythonAppVersion -ApplicationId $pythonAppID -Format zip -FilePath `
+    "tmp-resources/sample.zip"
+New-AzureRmBatchApplicationPackage -ResourceGroupName $rg -AccountName $batchAccount `
+    -ApplicationVersion $setupAppVersion -ApplicationId $setupAppID -Format zip -FilePath `
+    "tmp-resources/setup-python.zip"
+
+#  Set default version of the applications
+Set-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $pythonAppID `
+    -DefaultVersion $pythonAppVersion
+Set-AzureRmBatchApplication -ResourceGroupName $rg -AccountName $batchAccount -ApplicationId $setupAppID `
+    -DefaultVersion $setupAppVersion
+
+###  Configure Storage account
+
+#  Fetch storage account access keys
+$keys = Get-AzureRmStorageAccountKey -ResourceGroupName $rg -Name $storageAccount
+#  Create a context for storage account commands
+$context = New-AzureStorageContext -StorageAccountName $storageAccount -StorageAccountKey $keys.Value[0]
 
 #  Create a container for data files
 New-AzureStorageContainer -Name data -Context $context
+
+###  Clean temporary folder
+rmdir tmp-resources -Force -Recurse
+
