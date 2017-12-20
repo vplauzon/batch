43f92ac3da898a56f723b5daa64ae0cd35c14974
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2017-12-19 22:54:32 -0500

    .

--- PythonBatch/PythonBatchDeploy/DeployBatchComponents.ps1
+++ PythonBatch/PythonBatchDeploy/DeployBatchComponents.ps1
@@ -4,11 +4,11 @@
 ###  Replace the following variable values with yours
 
 #  Resource group name where the batch account is deployed
-$rg = "PythonBatch-2"
+$rg = "PythonBatch-4"
 #  Name of the batch account as specified when running the ARM template
-$batchAccount = "vplbatch2"
+$batchAccount = "vplbatch4"
 #  Name of the storage account as specified when running the ARM template
-$storageAccount = "vplbatchsto2"
+$storageAccount = "vplbatchsto4"
 
 ###  Those variable values do not need to be changed
 $pythonAppID = "PythonScript"
@@ -20,16 +20,18 @@ $setupAppVersion = "1.0"
 mkdir tmp-resources
 
 #  Copy resources locally
-(wget "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/Resources/sample.py").Content `
-    > tmp-resources/sample.py
-(wget "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/Resources/setup-python.sh").Content `
-    > tmp-resources/setup-python.sh
+Invoke-WebRequest -Uri `
+    "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/Resources/sample.py" `
+    -OutFile tmp-resources/sample.py
+Invoke-WebRequest -Uri `
+    "https://github.com/vplauzon/batch/raw/master/PythonBatch/PythonBatchDeploy/Resources/setup-python.sh" `
+    -OutFile tmp-resources/setup-python.sh
 
 #  Build an archive for each resource separately
 Compress-Archive -Path "tmp-resources\sample.py" -DestinationPath "tmp-resources/sample.zip" `
-    -CompressionLevel NoCompression
+    -CompressionLevel Optimal
 Compress-Archive -Path "tmp-resources\setup-python.sh" -DestinationPath "tmp-resources/setup-python.zip" `
-    -CompressionLevel NoCompression
+    -CompressionLevel Optimal
 
 ###  Configure Batch account
 
